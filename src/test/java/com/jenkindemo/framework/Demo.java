54560ad25aac794f82dee29303114fa2e0733846
Keshav057-max <keshav.057@gmail.com>
2022-12-21 10:19:08 +0530

    jenkins first

---
+++ src/test/java/com/jenkindemo/framework/Demo.java
@@ -0,0 +1,14 @@
+package com.jenkindemo.framework;
+
+import org.testng.annotations.Test;
+
+public class Demo {
+
+	@Test(groups="sanity")
+	public static void test1() {
+		System.out.println("welcome to Test yantra");
+		System.out.println("come to gc 10");
+
+	}
+
+}
